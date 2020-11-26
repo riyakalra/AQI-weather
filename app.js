89b8413f7576126317b2dfab22df3777e285ba25
namanbhatia <riyakalra59@gmail.com>
2020-11-26 16:13:04 +0530

    add procfile

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/reset", function (req, res) {
 })
 
 
-app.listen(3000, function (req, res) {
+app.listen(process.env.PORT || 3000, function (req, res) {
     console.log("Server running on port 3000");
 })
 
